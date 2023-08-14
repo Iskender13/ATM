d2d12ee7ac81a2101c653cf61a7e951e8393479f
Iskender13 <iskenorg@gmail.com>
2023-08-14 20:38:53 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(15000.0);
+        System.out.println("Initial balance: " + account.getAmount());
+
+        double withdrawalAmount = 6000.0;
+        while (true) {
+            try {
+                account.withdraw(withdrawalAmount);
+                System.out.println("Withdrawn: " + withdrawalAmount);
+            } catch (LimitException e) {
+                System.out.println("Remaining balance: " + e.getRemainingAmount());
+                break;
+            }
+        }
+    }
+}
